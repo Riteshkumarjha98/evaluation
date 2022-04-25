2efcaa4dad8908b08776ec70251de017db5ea88b
Riteshkumarjha98 <soumritjha@gmail.com>
2022-04-25 12:48:12 +0530

    file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,28 @@
 // Each ticket will cost 100 Rupees
 // If wallet amount is insufficient show alert "Insufficient Balance!";
 // Else show "Booking successfull!" and adjust the wallet amount in real time
+// Each ticket will cost 100 Rupees
+// If wallet amount is insufficient show alert "Insufficient Balance!";
+// Else show "Booking successfull!" and adjust the wallet amount in real time
+function myFunc(){
+    
+}
+
+
+let data = JSON.parse(localStorage.getItem("search"))||[];
+console.log(data)
+let mainDiv = document.getElementById("search");
+
+data.map(function(e,i){
+    let main = document.createElement("div");
+
+    let image = document.createElement("img")
+    image.src=e.Poster;
+    
+    let title = document.createElement("p");
+    title.innerText=e.Title;
+
+    main.append(image,title);
+    mainDiv.append(main)
+
+})
